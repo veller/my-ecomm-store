9b6af2b70b9933e8d5583bad9f8f0263bf51ed90
Veller <vellerbauer@gmail.com>
2021-01-25 15:43:36 -0300

    my ecomm store

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,41 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import products from '../products.json'
 
 export default function Home() {
+  console.log(products)
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Space Jelly Shop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Space Jelly Shop
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          The best space jellyfish swag on the web!
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.grid}>
+          {products.map(product => {
+            const {id, title, price, image, description} = product
+            return (
+              <li key={id} className={styles.card}>
+                <a href="https://nextjs.org/docs">
+                  <img src={image} alt={title}/>
+                  <h3>{title}</h3>
+                  <p>R${price}</p>
+                  <p>{description}</p>
+                </a>
+              </li>
+            )
+          })}          
+        </ul>
       </main>
 
       <footer className={styles.footer}>
